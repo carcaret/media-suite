efc6dcc716e12efa86654b5cd6884adbc6359460
Carlos Carbonell Etayo <carcaret@icloud.com>
2019-04-24 21:10:12 +0200

    Add basic auth

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,15 @@
 export PGID=$(id -u)
 export PUID=$(id -g)
 
-while getopts d:s:m: option
+while getopts d:s:m:u:p: option
 do
 	case "${option}"
 	in
 		d) DOWNLOADS_FOLDER=${OPTARG};;
 		s) SERIES_FOLDER=${OPTARG};;
 		m) MOVIES_FOLDER=${OPTARG};;
+		u) USERNAME=${OPTARG};;
+		p) PASSWORD=${OPTARG};;
 	esac
 done
 
@@ -28,8 +30,22 @@ then
 	MOVIES_FOLDER=./movies
 fi
 
+if [ -z "$USERNAME" ]
+then
+	USERNAME="admin"
+fi
+
+if [ -z "$PASSWORD" ]
+then
+	PASSWORD="admin"
+fi
+
 export DOWNLOADS_FOLDER=$DOWNLOADS_FOLDER
 export SERIES_FOLDER=$SERIES_FOLDER
 export MOVIES_FOLDER=$MOVIES_FOLDER
+
+cat > nginx/.auth << EOF
+$USERNAME:{PLAIN}$PASSWORD
+EOF
 
 docker-compose up
